beed22061c2ea4c7b601c05e1893ae9d50db1674
JamalAlabdullah <90609090+JamalAlabdullah@users.noreply.github.com>
2023-01-29 12:38:50 +0100

    Create githubAction-script.sh

---
+++ githubAction-script.sh
@@ -0,0 +1,11 @@
+          echo what is in this directory?
+          ls -a
+          echo
+          echo is Java installed?
+          java -version
+          echo is Git installed?
+          git --version
+          echo what about build tools?
+          gradle --version
+          ant -version
+          mvn -version
